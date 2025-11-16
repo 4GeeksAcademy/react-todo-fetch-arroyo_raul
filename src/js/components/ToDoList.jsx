53c8be89798d6f8bfb96dd27bb078303e71eed4c
arroyo-raul <arroyoguardiaraul@gmail.com>
2025-11-16 18:34:11 +0000

    ToDo con fetch

--- src/js/components/ToDoList.jsx
+++ src/js/components/ToDoList.jsx
@@ -1,36 +1,99 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const ToDoList = () => {
     const [tareas, setTareas] = useState([]);
+    const [inputValue, setInputValue] = useState("");
+
+    function obtenerValoresAPI() {
+        fetch('https://playground.4geeks.com/todo/users/arroyo_raul')
+            .then(resp => {
+                console.log(resp.ok)
+                console.log(resp.status);
+                return resp.json();
+            })
+            .then(data => {
+                console.log(data.todos);
+                setTareas(data.todos);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    function añadirTarea() {
+        const tarea = {
+            label: inputValue,
+            is_done: false,
+        }
+
+        fetch('https://playground.4geeks.com/todo/todos/arroyo_raul', {
+            method: 'POST',
+            body: JSON.stringify(tarea),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(resp => {
+                console.log(resp.ok);
+                console.log(resp.status);
+                return resp.json();
+            })
+            .then(data => {
+                console.log(data);
+                setTareas([...tareas, tarea]);
+                obtenerValoresAPI();
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
 
     function onKeyDown(event) {
         if (event.code === 'Enter') {
-            setTareas([...tareas, event.target.value]);
-            event.target.value = '';
+            añadirTarea();
+            event.target.value = "";
         }
     }
 
     function deleteTask(index) {
-        // recibimos el índice (lo que usabas como key) y eliminamos la tarea
-        setTareas(prev => prev.filter((_, i) => i !== index));
+        fetch(`https://playground.4geeks.com/todo/todos/${index}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(resp => {
+                console.log(resp.ok)
+                console.log(resp.status);
+                if(resp.ok === true) {
+                    obtenerValoresAPI();
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            })
     }
+
+    useEffect(function () {
+        obtenerValoresAPI();
+    }, []);
 
     return (
         <>
             <div className="contenedor">
                 <h2 className="title">Todo List</h2>
                 <div className="list">
-                    <input type="text" onKeyUp={onKeyDown} id="task" name="task" placeholder="Add any task" />
+                    <input type="text" onKeyUp={onKeyDown} onChange={(e) => { setInputValue(e.target.value) }} id="task" name="task" placeholder="Add any task" />
                     <ol>
-                        {tareas.map((tarea, index) => (
-                            <li key={index}><p>{tarea}<span onClick={() => deleteTask(index)}>X</span></p></li>
+                        {tareas.map((tarea) => (
+                            <li key={tarea.id}><p>{tarea.label}<span onClick={() => deleteTask(tarea.id)}>X</span></p></li>
                         ))}
                     </ol>
                     {tareas.length === 0 &&
-                    <div className="noTasksContainer">
-                        <p className="icono"><i class="fa-solid fa-x"></i></p> 
-                        <p className="noTasks">No tasks, add a task</p>
-                    </div>
+                        <div className="noTasksContainer">
+                            <p className="icono"><i className="fa-solid fa-x"></i></p>
+                            <p className="noTasks">No tasks, add a task</p>
+                        </div>
                     }
                     <p className="itemsLeft">{tareas.length} items left</p>
                 </div>
